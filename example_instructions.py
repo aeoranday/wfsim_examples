b9f2541f55d8a18176e4979e9f4401a5bdbdd1f1
Alex Oranday <alexoranday@gmail.com>
2020-07-10 14:13:24 -0500

    Some examples of WFSim setups

---
+++ example_instructions.py
@@ -0,0 +1,150 @@
+### Contain a set of example instructions that can be used to
+### replace the rand_instructions in wfsim.
+
+import wfsim.instruction_dtype
+import numpy as np
+
+def single_electron_instructions(c):
+    """
+    Instruction that is meant to simulate a single electron event.
+    This is met by setting the instruction amplitude to 1, which may
+    result in having 0 electrons (0 amplitude) in the simulated results.
+    """
+    n = c['nevents'] = c['event_rate'] * c['chunk_size'] * c['nchunk']
+    c['total_time'] = c['chunk_size'] * c['nchunk']
+
+    instructions = np.zeros(n, dtype=wfsim.instruction_dtype)
+    uniform_times = c['total_time'] * (np.arange(n) + 0.5) / n
+    instructions['time'] = np.repeat(uniform_times, 1) * int(1e9)
+
+    # Actually holds which chunk this event occurred in
+    instructions['event_number'] = np.digitize(instructions['time'],
+            1e9 * np.arange(c['nchunk']) * c['chunk_size']) - 1
+
+    instructions['type'] = np.tile([2], n) # S1 or S2
+    instructions['recoil'] = ['er' for i in range(n)] # Kind of recoil
+
+    # Random positioning
+    r = np.sqrt(np.random.uniform(0, c['tpc_radius']**2, n))
+    t = np.random.uniform(-np.pi, np.pi, n)
+    instructions['x'] = np.repeat(r * np.cos(t), 1)
+    instructions['y'] = np.repeat(r * np.sin(t), 1)
+    instructions['z'] = -10 * np.ones(n) # Choosing a constant depth
+
+    instructions['amp'] = np.ones(n) # Best attempt for single electron
+
+    return instructions
+
+def kr83m_instructions(c):
+    """
+    Instruction that is meant to simulate Kr83m events in the TPC.
+    """
+    import nestpy
+    half_life = 156.94e-9 # Kr intermediate state half-life in ns
+    decay_energies = [32.2, 9.4] # Decay energies in KeV
+
+    n = c['nevents'] = c['event_rate'] * c['chunk_size'] * c['nchunk']
+    c['total_time'] = c['chunk_size'] * c['nchunk']
+
+    # Uses 4*n to get the two energies for S1 and S2
+    instructions = np.zeros(4*n, dtype=wfsim.instruction_dtype)
+    instructions['event_number'] = np.digitize(instructions['time'],
+            1e9 * np.arange(c['nchunk']) * c['chunk_size']) - 1
+
+    instructions['type'] = np.tile([1,2], 2*n)
+    instructions['recoil'] = ['er' for i in range(4*n)]
+
+    # Random positioning
+    r = np.sqrt(np.random.uniform(0, c['tpc_radius']**2, n))
+    t = np.random.uniform(-np.pi, np.pi, n)
+    instructions['x'] = np.repeat(r * np.cos(t), 4)
+    instructions['y'] = np.repeat(r * np.sin(t), 4)
+    instructions['z'] = -10 * np.ones(4*n)
+    # Choosing shallow z positioning
+
+    # For correct times need to include the 156.94 ns half life of the intermediate state
+    uniform_times = c['total_time'] * (np.arange(n) + 0.5) / n
+    delayed_times = uniform_times + np.random.exponential(half_life/np.log(2),
+            len(uniform_times))
+    instructions['time'] = np.repeat(list(zip(uniform_times, delayed_times)), 2) * 1e9
+
+    # Defining XENON-like detector
+    nc = nestpy.NESTcalc(nestpy.VDetector())
+    A = 131.293
+    Z = 54
+    density = 2.862 # g/cm^3    # SR1 Value
+    drift_field = 82 # V/cm     # SR1 Value
+    interaction = nestpy.INTERACTION_TYPE(7) # gamma
+
+    energy = np.tile(decay_energies, n)
+    quanta = []
+    for en in energy:
+        y = nc.GetYields(interaction,
+                en,
+                density,
+                drift_field,
+                A,
+                Z,
+                (1,1))
+        quanta.append(nc.GetQuanta(y, density).photons)
+        quanta.append(nc.GetQuanta(y, density).electrons)
+
+    instructions['amp'] = quanta
+
+    return instructions
+
+def wall_instructions(c):
+    """
+    Instructions that are meant to focus on events near the walls of the TPC.
+    """
+    n = c['nevents'] = c['event_rate'] * c['chunk_size'] * c['nchunk']
+    c['total_time'] = c['chunk_size'] * c['nchunk']
+
+    instructions = np.zeros(n, dtype=wfsim.instruction_dtype)
+    uniform_times = c['total_time'] * (np.arange(n) + 0.5) / n
+    instructions['time'] = np.repeat(uniform_times, 1) * int(1e9)
+    instructions['event_number'] = np.digitize(instructions['time'],
+            1e9 * np.arange(c['nchunk']) * c['chunk_size']) - 1
+    instructions['type'] = np.tile([2], n)
+    instructions['recoil'] = ['er' for i in range(n)]
+
+    r = np.sqrt(np.random.uniform( (c['tpc_radius'] - 5)**2, c['tpc_radius']**2, n))
+    t = np.random.uniform(-np.pi, np.pi, n)
+    instructions['x'] = np.repeat(r * np.cos(t), 1)
+    instructions['y'] = np.repeat(r * np.sin(t), 1)
+    instructions['z'] = -10 * np.ones(n) # Constant depth choice
+
+    instructions['amp'] = np.arange(1, 1000, n)
+
+    return instructions
+
+def uniform_electrons_instructions(c):
+    """
+    Instructions that is similar to single_electron_instructions, but has a uniform distribution of
+    electrons from 1 to 1000. Should have the same pitfall as single_electron_instructions where a
+    1 electron could simulate a 0 amplitude/electron event.
+    """
+    n = c['nevents'] = c['event_rate'] * c['chunk_size'] * c['nchunk']
+    c['total_time'] = c['chunk_size'] * c['nchunk']
+
+    instructions = np.zeros(n, dtype=wfsim.instruction_dtype)
+    uniform_times = c['total_time'] * (np.arange(n) + 0.5) / n
+    instructions['time'] = np.repeat(uniform_times, 1) * int(1e9)
+
+    # Actually holds which chunk this event occurred in
+    instructions['event_number'] = np.digitize(instructions['time'],
+            1e9 * np.arange(c['nchunk']) * c['chunk_size']) - 1
+
+    instructions['type'] = np.tile([2], n) # S1 or S2
+    instructions['recoil'] = ['er' for i in range(n)] # Kind of recoil
+
+    # Random positioning
+    r = np.sqrt(np.random.uniform(0, c['tpc_radius']**2, n))
+    t = np.random.uniform(-np.pi, np.pi, n)
+    instructions['x'] = np.repeat(r * np.cos(t), 1)
+    instructions['y'] = np.repeat(r * np.sin(t), 1)
+    instructions['z'] = -10 * np.ones(n) # Choosing a constant depth
+
+    instructions['amp'] = np.random.uniform(1, 1000, n) # Best attempt for single electron
+
+    return instructions
---
+++ simulate.py
@@ -0,0 +1,100 @@
+# General file for gathering the simulation data from WFSim
+
+import wfsim
+import strax
+import straxen
+import sys
+import argparse
+
+from example_instructions import single_electron_instructions
+import numpy as np
+import pickle as pk
+
+AUX_REPO = 'https://raw.githubusercontent.com/XENONnT/strax_auxiliary_files/'
+CONFIG_1T_HASH = '8b3c60a1d790dbcc9103559d0e019008b8e760dc/' # Last commit was June 26, 2020
+CONFIG_nT_HASH = '4e71b8a2446af772c83a8600adc77c0c3b7e54d1/' # Last commit was June 24, 2020
+GAIN_HASH = '58e615f99a4a6b15e97b12951c510de91ce06045/'      # Last commit was Dec, 2019
+
+
+# Take the run_id from command line argument (first arg)
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-r', '--run_id', type=int, required=True,
+        help="run_id that is also used for the random seed")
+parser.add_argument('-d', '--detector', type=str, required=True,
+        help="choose the detector type: nt or 1t")
+
+# Although an argument could be added for which instructions to use, I choose to leave it out. The
+# instruction functions are specific to what is wanting to be simulated. For more complex
+# instructions, it should be upon the user to define this instruction and import it into here.
+
+args = parser.parse_args()
+detector = args.detector
+run_id = str(args.run_id)
+
+np.random.seed( int(run_id) )
+
+if "1t" in detector.lower():
+    st = strax.Context(storage=strax.DataDirectory('./strax_data'),
+                       register=wfsim.RawRecordsFromFax,
+                       config=dict(detector='XENON1T',
+                                  fax_config=AUX_REPO+CONFIG_1T_HASH+'fax_files/fax_config_1t.json',
+                                  **straxen.contexts.x1t_common_config),
+                       timeout=3600,
+                       **straxen.contexts.common_opts)
+    DETECTOR='1T'
+elif "nt" in detector.lower():
+    straxen.contexts.xnt_common_config['gain_model'] = ('to_pe_per_run',
+            AUX_REPO+GAIN_HASH+'fax_files/to_pe_nt.npy')
+    st = strax.Context(storage=strax.DataDirectory('./strax_data'),
+                       register=wfsim.RawRecordsFromFax,
+                       config=dict(detector='XENONnT',
+                                  fax_config=AUX_REPO+CONFIG_nT_HASH+'fax_files/fax_config_nt.json',
+                                  to_pe_file=AUX_REPO+GAIN_HASH+'fax_files/to_pe_nt.npy',
+                                  **straxen.contexts.xnt_common_config),
+                       timeout=3600,
+                       **straxen.contexts.common_opts)
+    DETECTOR='nT'
+else:
+    from sys import exit
+    exit("Detector option give was not one of the ones available: nT or 1T.")
+
+# Replacing wfsim's built-in rand_instructions
+wfsim.strax_interface.rand_instructions = single_electron_instructions # Single electron requires filtering of 0 electron events
+
+st.set_config(dict(fax_files=None, nchunk=100, event_rate=1, chunk_size=500))
+
+# Wanting perfect
+st.set_config(dict(field_distortion_on=False))
+
+peaks = st.get_array(run_id, 'peaks')
+truth = st.get_array(run_id, 'truth')
+basics = st.get_array(run_id, 'peak_basics')
+
+# There will be various truths that are not type 2 (not S2)
+good_truth = truth[truth['type'] == 2]
+good_truth = good_truth[good_truth['n_electron'] == 1] # Want single electrons only
+
+# Removing the non-S2s gets events out of sync, so I'll match them based on times
+good_pk_mask = np.zeros(len(basics), dtype=np.bool)
+for i, t in enumerate(good_truth):
+    good_pk_mask += ( (basics['time'] < t['t_mean_photon']) &
+                      (basics['endtime'] > t['t_mean_photon']) )
+
+peaks = peaks[good_pk_mask]
+
+hit_pattern_top = peaks['area_per_channel'][:,:253]
+event_pos = np.vstack( (good_truth['x'], good_truth['y']) ).T
+#events = np.zeros(len(event_pos),
+#                  dtype=[('area_per_channel', np.float32, hit_pattern_top.shape[1]),
+#                         ('true_pos', np.float32, 2)])
+
+events = {}
+events['area_per_channel'] = hit_pattern_top
+events['true_pos'] = event_pos
+events['true_time'] = good_truth['time']
+events['sim_time'] = peaks['time']
+
+with open('./pickles/{}-50000_single-el_z-10_sim_{}.pkl'.format(run_id, DETECTOR), 'wb') as fn:
+    pk.dump(events, fn)
+
